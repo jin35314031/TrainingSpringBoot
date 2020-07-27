68b49aed3d536d2aea83ee7c38f3a9695348c472
jin35314031 <jin35314031@gmail.com>
2020-07-27 16:24:04 +0900

    listDisplay

---
+++ todo-api/src/test/java/com/example/todoapi/TodoApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.todoapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TodoApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
